967c56383fa31817047692c687895a6d951f17f5
rodeloescueta <dummy@gmail.com>
2020-02-27 14:11:19 +0800

    first deploy

---
+++ precache-manifest.0b03fb4fb0ad0661a4cf277c870516da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/my-project/robots.txt"
+  },
+  {
+    "revision": "a5648ceab56e40f2600c",
+    "url": "/my-project/js/chunk-vendors.8e085efe.js"
+  },
+  {
+    "revision": "6a3aebb1972e471b81c9",
+    "url": "/my-project/js/app.7cfb2a03.js"
+  },
+  {
+    "revision": "2783442a41f8dccb0d3d",
+    "url": "/my-project/js/about.3b05a822.js"
+  },
+  {
+    "revision": "22145011b1a9b01c51eaa077256909c7",
+    "url": "/my-project/index.html"
+  },
+  {
+    "revision": "a5648ceab56e40f2600c",
+    "url": "/my-project/css/chunk-vendors.12811a77.css"
+  }
+];
